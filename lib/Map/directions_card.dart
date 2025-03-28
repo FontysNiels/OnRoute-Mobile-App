1b27bb17b68b55e34a66e45fe468449ad708c8bd
FontysNiels <nielsfeijen76@gmail.com>
2025-03-28 08:35:19 +0100

    code cleanup

--- lib/Map/directions_card.dart
+++ lib/Map/directions_card.dart
@@ -25,26 +25,28 @@ class DirectionsCard extends StatefulWidget {
 }
 
 class _DirectionsCardState extends State<DirectionsCard> {
-  int metersToNextDirection = 0;
-  int i = 0;
+  int metersToCurrentDirection = 0;
+  int descriptionNum = 0;
+  int amountOfDirections = 0;
   bool skipNextCheck = false;
-  late final List<DescriptionPoint> _directionsList;
+  late final List<DescriptionPoint> directionList;
 
   @override
   void initState() {
     // TODO: implement initState
-    _directionsList = [];
-    for (var i = 0; i < widget._directionsList.length; i++) {
+    amountOfDirections = widget._directionsList.length;
+    directionList = [];
+    for (var i = 0; i < amountOfDirections; i++) {
       // _directionsList.add(widget._directionsList[i]);
       if (i > 0) {
         if (i == 1) {
-          _directionsList.add(widget._directionsList[i]);
+          directionList.add(widget._directionsList[i]);
         } else if (widget._directionsList[i].x ==
                 widget._directionsList[i - 1].x &&
             widget._directionsList[i].y == widget._directionsList[i - 1].y) {
-          _directionsList.add(widget._directionsList[i - 1]);
+          directionList.add(widget._directionsList[i - 1]);
         } else {
-          _directionsList.add(widget._directionsList[i]);
+          directionList.add(widget._directionsList[i]);
         }
       }
     }
@@ -59,8 +61,8 @@ class _DirectionsCardState extends State<DirectionsCard> {
             widget._mapViewController.locationDisplay.location!.position;
 
         final directionPointXY = convertToLatLng(
-          _directionsList[i].x,
-          _directionsList[i].y,
+          directionList[descriptionNum].x,
+          directionList[descriptionNum].y,
         );
         final directionPointX = directionPointXY[1];
         final directionPointY = directionPointXY[0];
@@ -73,48 +75,13 @@ class _DirectionsCardState extends State<DirectionsCard> {
         // Define a threshold distance (e.g., 10 meters)
         // Approx. 10 meters in lat/long degrees
         const thresholdDistance = 0.0001;
-
         const metersPerDegree = 111320; // Approximation for latitude
         final distanceInMeters = distanceToDirectionPoint * metersPerDegree;
 
-        // Distance between the user and the next point
-        final directionPointXYnext = convertToLatLng(
-          _directionsList[i == _directionsList.length - 1 ? i : i + 1].x,
-
-          _directionsList[i == _directionsList.length - 1 ? i : i + 1].y,
-        );
-        final directionPointXnext = directionPointXYnext[1];
-        final directionPointYnext = directionPointXYnext[0];
-
-        final distanceNext = sqrt(
-          pow(userPosition.y - directionPointYnext, 2) +
-              pow(userPosition.x - directionPointXnext, 2),
-        );
-
-        final distanceInMeterToNext = distanceNext * metersPerDegree;
-
-        // Distance between the current and next point
-        var distanceBetweenPoints = 0;
-        final matchingFeatures = widget._routeInfo.layers[1].featureSet.features
-            .where(
-              (feature) => feature.attributes['DirectionPointID'] == (i + 1),
-            );
-
-        if (matchingFeatures.isNotEmpty) {
-          distanceBetweenPoints =
-              matchingFeatures.first.attributes['Meters'].toInt();
-        } else {
-          // distanceBetweenPoints = 0; // Default value if no matching feature is found
-        }
-        // print("points distance: $distanceBetweenPoints");
-        // print('nect distance $distanceInMeterToNext');
-        // print('current distance $distanceInMeters');
-
         // Create a list of descriptions and distances
-        List<Map<String, dynamic>> descriptionsAndDistances = [];
-
-        for (int index = 0; index < _directionsList.length; index++) {
-          final point = _directionsList[index];
+        List<Map<String, dynamic>> descriptionDistanceList = [];
+        for (int index = 0; index < directionList.length; index++) {
+          final point = directionList[index];
           final pointXY = convertToLatLng(point.x, point.y);
           final pointX = pointXY[1];
           final pointY = pointXY[0];
@@ -126,6 +93,7 @@ class _DirectionsCardState extends State<DirectionsCard> {
           final distanceInMetersToPoint = distanceToPoint * metersPerDegree;
 
           int distanceBetweenPoints = 0;
+          // Get distance between points
           final matchingFeatures = widget
               ._routeInfo
               .layers[1]
@@ -136,94 +104,61 @@ class _DirectionsCardState extends State<DirectionsCard> {
                     feature.attributes['DirectionPointID'] == (index + 1),
               );
 
+          // Set distance between points
           if (matchingFeatures.isNotEmpty) {
             distanceBetweenPoints =
                 matchingFeatures.first.attributes['Meters'].toInt();
           }
-          descriptionsAndDistances.add({
-            // 'description': point.description,
+          descriptionDistanceList.add({
             'userDistanceFromPoint': distanceInMetersToPoint.toInt(),
-            'distanceToNext': distanceBetweenPoints,
+            'distanceBetweenPoints': distanceBetweenPoints,
           });
         }
 
-        // Print the list for debugging
-        print(descriptionsAndDistances.length);
-        print(_directionsList.length);
-
         // MAKE IT SO IT CHECK IF WALKING AWAY (_mapViewController.locationDisplay.location!.course)
         if (distanceToDirectionPoint < thresholdDistance) {
-          if (i < widget._directionsList.length - 1) {
+          if (descriptionNum < amountOfDirections - 1) {
             print("User has passed the current direction's coordinates.");
             setState(() {
-              i++;
+              descriptionNum++;
               skipNextCheck = true; // Skip the next check
               // ^ uit setstate zetten?
             });
           }
         } else if (!skipNextCheck) {
-          if (metersToNextDirection < distanceInMeters.toInt() &&
-              metersToNextDirection != 0) {
-            print('distance increasing');
+          // Check if the distance to current direction is increasing
+          if (metersToCurrentDirection < distanceInMeters.toInt() &&
+              metersToCurrentDirection != 0) {
+            // If so, check if user is getting closer to a different point
             for (
               var iLoop = 0;
-              iLoop < descriptionsAndDistances.length;
+              iLoop < descriptionDistanceList.length;
               iLoop++
             ) {
-              if (iLoop != descriptionsAndDistances.length - 1) {
-                //
-                // if (iLoop <= i) {
-                //   continue;
-                // }
-
-                if (descriptionsAndDistances[iLoop +
+              // Can't check the one after the last
+              if (iLoop != descriptionDistanceList.length - 1) {
+                // Check if user is closer than the set distance to it
+                if (descriptionDistanceList[iLoop +
                         1]['userDistanceFromPoint'] <
-                    descriptionsAndDistances[iLoop]['distanceToNext']) {
-                  if (i > iLoop) {
+                    descriptionDistanceList[iLoop]['distanceBetweenPoints']) {
+                  // Skip if the user is getting closer to one that they already passed
+                  if (descriptionNum > iLoop) {
                     continue;
                   }
-                  print('closer to next point');
-                  // print(descriptionsAndDistances[iLoop]);
-
-                  // descriptionsAndDistances[iLoop]['distanceToNext'] =
-                  //     descriptionsAndDistances[iLoop + 1]['distanceToNext'];
-                  // descriptionsAndDistances[iLoop + 1]['distanceToNext'] = 0;
-                  // }
-
-                  // print(widget._directionsList[i].description);
-                  // print(descriptionsAndDistances[iLoop]['distanceToNext']);
-                  // print(metersToNextDirection);
-                  // print(distanceInMeters.toInt());
-
-                  i++;
+                  // Change to the next description and stop the loop
+                  descriptionNum++;
                   break;
                 }
               }
             }
           }
-          // if user walks away from point and is getting closer to next point (start point test only, so if you start halfway it doesnt work)
-          // if (metersToNextDirection < distanceInMeters.toInt() &&
-          //     distanceBetweenPoints > distanceInMeterToNext &&
-          //     metersToNextDirection != 0 &&
-          //     distanceBetweenPoints != 0) {
-          //   if (i < widget._directionsList.length - 1) {
-          //     i++;
-          //     print(
-          //       "${widget._directionsList[i].description} : Significant decrease in distance detected.",
-          //     );
-          //   }
-          // }
         } else {
           skipNextCheck = false; // Reset the flag after skipping
         }
 
-        // DEZE IN DE UITLOOPCHECK ZETTEN, ALS HET BLIJFT UITLOPEN ZAL HIJ DIE OLD UPDATEN EN DUS IS EEN MINIMAAL LASTIG TE ZETTEN
-        // DUS ALLEEN OLD AANPASSEN ALS HET NIET WEGLOOPT (ALS IK EEN MARGIN WIL HEBBEN VAN EEN BEPAALDE AFSTAND)
-        // IDK OF DAT WERKT, WANT DELAYED DAN (maybe een if > distanceInMeters meer dan 50 ofzo)
-
         // Update distance to nect direction
         setState(() {
-          metersToNextDirection = distanceInMeters.toInt();
+          metersToCurrentDirection = distanceInMeters.toInt();
         });
       }
     });
@@ -252,14 +187,14 @@ class _DirectionsCardState extends State<DirectionsCard> {
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 12.0),
                   child: Text(
-                    _directionsList.isNotEmpty
-                        ? _directionsList[i].description
+                    directionList.isNotEmpty
+                        ? directionList[descriptionNum].description
                         : "NIKS",
                   ),
                 ),
               ),
               // Text('$metersToNextDirection Meter'),
-              Text('${metersToNextDirection} m'),
+              Text('${metersToCurrentDirection} m'),
             ],
           ),
         ),
