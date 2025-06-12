790fb74539abf82f16e42ec6bee672107e7756d5
FontysNiels <nielsfeijen76@gmail.com>
2025-06-12 12:57:39 +0200

    package_image_preview -> route_image_preview

--- lib/Components/BottomSheet/Single-Route/route_image_preview.dart
+++ lib/Components/BottomSheet/Single-Route/route_image_preview.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:onroute_app/Classes/web_map_collection.dart';
 
-class PackegImagePreview extends StatelessWidget {
+class RouteImagePreview extends StatelessWidget {
   final WebMapCollection routeContent;
-  const PackegImagePreview({super.key, required this.routeContent});
+  const RouteImagePreview({super.key, required this.routeContent});
 
   @override
   Widget build(BuildContext context) {
